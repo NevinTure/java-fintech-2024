e412bfffa12188548aaedba556a3923565450fde
NevinTure <nikitastar18.b@yandex.ru>
2024-09-22 15:01:37 +0300

    add dependencies

---
+++ kudago-api/src/main/java/edu/java/kudagoapi/KudagoApiApplication.java
@@ -0,0 +1,13 @@
+package edu.java.kudagoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KudagoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KudagoApiApplication.class, args);
+    }
+
+}
