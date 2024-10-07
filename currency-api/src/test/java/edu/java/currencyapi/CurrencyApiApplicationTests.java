68e4ec72ce07486304b90b69475c455199f0a3d3
NevinTure <nikitastar18.b@yandex.ru>
2024-10-07 19:03:28 +0300

    add currency-api module

---
+++ currency-api/src/test/java/edu/java/currencyapi/CurrencyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.java.currencyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
