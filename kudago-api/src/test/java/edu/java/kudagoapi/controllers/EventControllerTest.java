070980b07bf90d9692549f660b8f900469a2e0d9
NevinTure <nikitastar18.b@yandex.ru>
2024-10-13 19:50:11 +0300

    add EventController tests

--- kudago-api/src/test/java/edu/java/kudagoapi/controllers/EventControllerTest.java
+++ kudago-api/src/test/java/edu/java/kudagoapi/controllers/EventControllerTest.java
@@ -1,79 +1,140 @@
 package edu.java.kudagoapi.controllers;
 
-import edu.java.kudagoapi.IntegrationEnvironment;
-import edu.java.kudagoapi.clients.CurrencyClient;
-import edu.java.kudagoapi.clients.KudagoClient;
-import edu.java.kudagoapi.dtos.*;
-import edu.java.kudagoapi.dtos.events.EventsResponse;
+import edu.java.kudagoapi.dtos.LocationDto;
+import edu.java.kudagoapi.dtos.events.*;
+import edu.java.kudagoapi.exceptions.BadRequestApiException;
 import edu.java.kudagoapi.services.EventService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
+import reactor.core.publisher.Mono;
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.*;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class EventControllerTest extends IntegrationEnvironment {
+public class EventControllerTest {
 
-    @Autowired
-    private KudagoClient kudagoClient;
-    @Autowired
-    private CurrencyClient currencyClient;
-    @Autowired
-    private EventService eventService;
+    @MockBean
+    private EventService service;
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void test() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(get("/api/v1/events/reactor")
-                        .queryParam("budget", "100")
-                        .queryParam("currency", "RUB")
-                        .queryParam("dateFrom", "2024-02-12")
-                        .queryParam("dateTo", "2024-10-12"))
-                .andExpect(status().isOk())
-                .andReturn();
-        mockMvc
-                .perform(asyncDispatch(mvcResult))
-                        .andExpect(status().isOk())
-                                .andDo(print());
-    }
-
-    @Test
-    public void test2() throws Exception {
-        LocalDate from = LocalDate.of(2024, 2, 12);
-        LocalDate to = LocalDate.of(2024, 10, 12);
-        Map<String, String> request = new HashMap<>();
-        request.put("fields", "id,place,location,price");
-        request.put("expand", "place,location");
-        request.put("actual_since", from.toString());
-        request.put("actual_until", to.toString());
-        request.put("page_size", "100");
-        EventsResponse events = kudagoClient.getEvents(request);
-        System.out.println(events);
-
-    }
-
-    @Test
-    public void test3() {
-        CurrencyConvertRequest request = new CurrencyConvertRequest(
-                "RUB",
-                "RUB",
-                new BigDecimal(100)
+    public void testGetEventWithFuture() throws Exception {
+        //when
+        List<EventDto> events = List.of(
+                new EventDto(1L, new PlaceDto(), new LocationDto(), "200"),
+                new EventDto(2L, new PlaceDto(), new LocationDto(), "300")
         );
-        CurrencyConvertResponse response = currencyClient.convert(request);
-        System.out.println(response);
+        Mockito.when(service
+                        .getEventsWithFuture(new BigDecimal(5), "USD", null, null))
+                .thenReturn(CompletableFuture
+                        .completedFuture(new EventsResponse(events)));
+
+        //then
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/events/future")
+                        .queryParam("budget", "5")
+                        .queryParam("currency", "USD"))
+                .andReturn();
+        mockMvc.perform(asyncDispatch(mvcResult))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        """
+                                {
+                                  "results": [
+                                                {
+                                                "id": 1,
+                                                "place": {},
+                                                "location": {},
+                                                "price": "200"
+                                                },
+                                                {
+                                                  "id": 2,
+                                                  "place": {},
+                                                  "location": {},
+                                                  "price": "300"
+                                                }
+                                             ]
+                                }
+                                """));
+    }
+
+    @Test
+    public void testGetEventWithFutureWhenError() throws Exception {
+        //when
+        Mockito.when(service
+                        .getEventsWithFuture(new BigDecimal(-5), "USD", null, null))
+                .thenThrow(BadRequestApiException.class);
+
+        //then
+        mockMvc.perform(get("/api/v1/events/future")
+                        .queryParam("budget", "-5")
+                        .queryParam("currency", "USD"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testGetEventWithReactor() throws Exception {
+        //when
+        List<EventDto> events = List.of(
+                new EventDto(1L, new PlaceDto(), new LocationDto(), "200"),
+                new EventDto(2L, new PlaceDto(), new LocationDto(), "300")
+        );
+        Mockito.when(service
+                        .getEventsWithReactor(new BigDecimal(5), "USD", null, null))
+                .thenReturn(Mono.just(new EventsResponse(events)));
+
+        //then
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/events/reactor")
+                        .queryParam("budget", "5")
+                        .queryParam("currency", "USD"))
+                .andReturn();
+        mockMvc.perform(asyncDispatch(mvcResult))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        """
+                                {
+                                  "results": [
+                                                {
+                                                "id": 1,
+                                                "place": {},
+                                                "location": {},
+                                                "price": "200"
+                                                },
+                                                {
+                                                  "id": 2,
+                                                  "place": {},
+                                                  "location": {},
+                                                  "price": "300"
+                                                }
+                                             ]
+                                }
+                                """));
+    }
+
+    @Test
+    public void testGetEventWithReactorWhenError() throws Exception {
+        //when
+        Mockito.when(service
+                        .getEventsWithReactor(new BigDecimal(-5), "USD", null, null))
+                .thenThrow(BadRequestApiException.class);
+
+        //then
+        mockMvc.perform(get("/api/v1/events/reactor")
+                        .queryParam("budget", "-5")
+                        .queryParam("currency", "USD"))
+                .andExpect(status().isBadRequest());
     }
 }
-//?expand=place%2Clocation&fields=id%2Cplace%2Clocation%2Cprice&page_size=100&actual_since=2024-02-12&actual_until=2024-10-12"
---
+++ kudago-api/src/test/java/edu/java/kudagoapi/services/EventServiceTest.java
@@ -0,0 +1,7 @@
+package edu.java.kudagoapi.services;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EventServiceTest {
+}
