709a9ead8b8e2b4be723ae8f94380a2fc67a11e5
NevinTure <nikitastar18.b@yandex.ru>
2024-10-24 23:56:40 +0300

    disable datasource autoconfig

--- kudago-api/src/main/java/edu/java/kudagoapi/KudagoApiApplication.java
+++ kudago-api/src/main/java/edu/java/kudagoapi/KudagoApiApplication.java
@@ -3,11 +3,15 @@ package edu.java.kudagoapi;
 import edu.java.kudagoapi.configuration.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(exclude =
+        {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableFeignClients
 @EnableAsync
 @EnableConfigurationProperties(ApplicationConfig.class)
